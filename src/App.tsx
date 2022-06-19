fb2b5a29062d2af90c0e1eb72205736537a5b6e1
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2022-06-19 18:52:16 +0200

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React, {MouseEvent,useState} from 'react';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
 import {Button} from "./components/button";
+import {ALL} from "dns";
+import {NewComponent} from "./NewComponent";
+
 
 function App() {
-    //let a = 1
-    let[a, setA]=useState(1)
 
-    const onClickHandler = () => {
-        setA(++a)
-        console.log(a)
-    }
-    const OnClickNull = ( ) => {
-      setA(a=0);
-    }
 
-    return (
-        <div className="App">
-            <h1> {a}</h1>
-            <button onClick={onClickHandler}>number</button>
-            <button onClick={OnClickNull}>0</button>
-        </div>
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'Hryvna', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'Hryvna', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'Hryvna', value: 50, number: ' v1234567890'},
+    ])
+
+    let [button, defineBtn]=useState('')
+
+    let currentMoney = money;
+
+    if (button==='Dollars'){currentMoney = money.filter((el)=>el.banknots==='Dollars')}
+    else if(button==='Hryvna'){currentMoney = money.filter((el)=>el.banknots==='Hryvna')}
+
+
+
+    const onClickHandlerButton=(e: React.MouseEvent<HTMLButtonElement>)=>{
+        const btn = e.currentTarget.innerText
+       defineBtn(button = btn)
+    }
+    return(
+        <NewComponent  banknots={currentMoney} onClickHandler={onClickHandlerButton} />
     )
+
 }
 
 export default App;
